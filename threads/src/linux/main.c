ccd37259660b4229d45dc565dfd2a3eab44884aa
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-01-10 22:24:05 +0100

    threads: Add time based simple scheduler implementation
    - Basic thread switching only

--- threads/src/linux/main.c
+++ threads/src/linux/main.c
@@ -1,5 +1,7 @@
+#include <signal.h>
 #include <unistd.h>
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +10,7 @@
 
 // MACROS
 
-#define ARRAY_SIZE(arr) sizeof(arr) / sizeof(arr[0])
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 #define UNUSED_PARAM __attribute__((unused))
 
 // %rsp	stack pointer	Yes
@@ -41,13 +43,18 @@ typedef void (*thread_entry_f)(void);
 
 // PRIVATE VARIABLES
 
-struct thread *threads[2];
+struct stack_frame main_frame = {0};
+struct thread main_thread = {
+    .context = &main_frame,
+};
+static struct thread *threads[2];
+static bool should_stop = false;
 
 // FUNCTION DEFINITIONS
 
 struct thread *thread_create(thread_entry_f handler)
 {
-    const size_t default_stack_size = 1024;
+    const size_t default_stack_size = 4096;
     const size_t to_allocate = default_stack_size + sizeof(struct thread);
     uint8_t *memory = calloc(1, to_allocate);
 
@@ -89,8 +96,6 @@ void thread_switch(struct thread *from, struct thread *to) {
 }
 
 noreturn void handler1(void) {
-    uint8_t counter = 1;
-
     for (;;) {
         // push rip = handler1
         // push rbp, rdx, r12-15
@@ -98,44 +103,78 @@ noreturn void handler1(void) {
         // pop rbp, rdx, r12-15
         // pop rip = handler1
         sleep(2);
-
-        if (counter == 3) {
-            counter = 0;
-            thread_switch(threads[0], threads[1]);
-        }
-
-        ++counter;
     }
 }
 
 noreturn void handler2(void) {
-    uint8_t counter = 1;
-
     for (;;) {
         printf("Hello from thread 2\n");
         sleep(2);
+    }
+}
 
-        if (counter == 3) {
-            counter = 0;
-            thread_switch(threads[1], threads[0]);
-        }
+void handler(int sig_num) {
+    static size_t counter = 0;
+    static size_t iteration = 0;
 
-        ++counter;
+    if (sig_num != SIGALRM) {
+        return;
+    }
+
+    const size_t current_thread_id = counter % ARRAY_SIZE(threads);
+    const size_t next_thread_id = (counter + 1) % ARRAY_SIZE(threads);
+
+    ++iteration;
+    ++counter;
+    printf("Schedule switching from %zu to %zu\n", current_thread_id, next_thread_id);
+    //    thread_switch(threads[current_thread_id], threads[next_thread_id]);
+
+    if (iteration < 2) {
+        alarm(2);
+        thread_switch(threads[current_thread_id], threads[next_thread_id]);
+    }
+    else {
+        should_stop = true;
+        thread_switch(threads[current_thread_id], &main_thread);
+    }
+}
+
+void *get_pc(void) {
+    return __builtin_return_address(0);
+}
+
+void assert(bool expression) {
+    if (!expression) {
+        __asm__("int3");
     }
 }
 
 int main() {
+    struct sigaction sa = {0};
+
+    sa.sa_handler = handler;
+    sa.sa_flags = SA_NODEFER;
+
+    sigaction(SIGALRM, &sa, NULL);
     threads[0] = thread_create(handler1);
     threads[1] = thread_create(handler2);
 
-    struct stack_frame main_frame = {0};
-    struct thread main_thread = {
-        .context = &main_frame,
-    };
-
     printf("Allocated thread 1 address: %p\n", threads[0]);
     printf("Allocated thread 2 address: %p\n", threads[1]);
+    alarm(1);
+    // TODO: check how to setup proper offset to jump over the `thread_switch()` call
+    main_frame.rip = (uint64_t)(get_pc()) + 1;
     thread_switch(&main_thread, threads[0]);
+
+    for (;;) {
+        if (should_stop) {
+            break;
+        }
+
+        sleep(2);
+    }
+
+    printf("Goodbye!");
     thread_destroy(threads[0]);
     thread_destroy(threads[1]);
 
